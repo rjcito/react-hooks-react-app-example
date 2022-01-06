3fd2b86a158b4820059a17c7bf575c66c37e9437
Roberto Jacquez <rjqz92@gmail.com>
2022-01-05 17:09:34 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+//Function comes from the react-dom npm package. It takes in two arguments
+// 1. A React component to render (typically, we'll render our top-level App component here)
+// 2. A DOM Element where we want that component to be rendered (by convention, a div with the ID of root).
